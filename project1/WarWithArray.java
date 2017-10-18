2483c6792631d7dc29fc88d0b388cbb6b7758e3d
mwray <mewray@iastate.edu>
2017-10-18 14:29:48 -0500

    Implemented WarWithArray class. Have not tested yet.

--- project1/WarWithArray.java
+++ project1/WarWithArray.java
@@ -12,16 +12,68 @@ import java.util.ArrayList;
 
 public class WarWithArray
 {
-	// member fields and methods
+	String[] s;
+	int k;
 	
 	public WarWithArray(String[] s, int k)
 	{
-		// implementation
+		this.s = s;
+		this.k = k;
 	}
 	
+	/**
+	 * Computes all 2k-length possible substring combinations of the elements in s
+	 * @return An ArrayList containing the 2k-length strings.
+	 */
 	public ArrayList<String> compute2k()
 	{
-		// implementation
+		ArrayList<String> t = new ArrayList<String>();
+		
+		for(String e : s)
+		{
+			for(String f : s)
+			{
+				if(isValid(e + f))
+				{
+					t.add(e + f);
+				}
+			}
+		}
+		return t;
+	}
+	
+	/**
+	 * Iterates through the given string, checking if each k-length substring is in s.
+	 * @param a The string to check
+	 * @return True if all k-length substrings are in s, false otherwise.
+	 */
+	private boolean isValid(String a)
+	{
+		for(int i = 0; i <= k; i++)
+		{
+			if(!exists(a.substring(i, i+k)))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Iterates through the array s, searching for a given string.
+	 * @param a The string to search for.
+	 * @return True is the string is in the array, false if it is not.
+	 */
+	private boolean exists(String a)
+	{
+		for(String b : s)
+		{
+			if(a.equals(b))
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
